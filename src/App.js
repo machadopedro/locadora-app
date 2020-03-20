be8f3b9885bcd8d5f739ec49fe219f1713f9ad41
Pedro Machado <machado.pedro11@gmail.com>
2020-03-20 18:02:24 -0300

    Fixed api address

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import { NavLink, Switch, Route } from 'react-router-dom';
 import { List, Button, Form, Input, Checkbox} from 'semantic-ui-react';
 
-const backpath = 'https://4wordg1lp0.execute-api.us-east-1.amazonaws.com
+const backpath = 'https://4wordg1lp0.execute-api.us-east-1.amazonaws.com'
 
 function App() {
   const [user, setUser] = useState('guest');
