aacbd8e92a84c5ef4d8c3cbbe8d1782af0ea8bb1
Pedro Machado <machado.pedro11@gmail.com>
2020-03-20 17:03:27 -0300

    Updates

---
+++ precache-manifest.3cf4343674263ba004858550485bff96.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5da3fa11d7d3e5e9c9b8fe3ed6bb5480",
+    "url": "/locadora-app/index.html"
+  },
+  {
+    "revision": "c481b0b8bc6aa78cf0e5",
+    "url": "/locadora-app/static/css/main.79c14169.chunk.css"
+  },
+  {
+    "revision": "ce1f811f6d533209be95",
+    "url": "/locadora-app/static/js/2.14c8192b.chunk.js"
+  },
+  {
+    "revision": "49c45e15b22906ea61e3b3085d227f58",
+    "url": "/locadora-app/static/js/2.14c8192b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c481b0b8bc6aa78cf0e5",
+    "url": "/locadora-app/static/js/main.c3a35b5d.chunk.js"
+  },
+  {
+    "revision": "586bbf6facaa839f4c2b",
+    "url": "/locadora-app/static/js/runtime-main.fd2db313.js"
+  }
+]);
